105c106605e3ded1921e7cb64a7173a279f6edd7
manolo <emmanuelripoll@gmail.com>
2017-01-06 14:36:49 +0100

    last version

--- entete.php
+++
@@ -1,3 +0,0 @@
-<div id="en_tete">
-		<a href="index.php"><img src="photos/logo-glisseurbaine.jpg" alt="Glisse Urbaine" class="logo GU" title="Glisseurbaine.fr"/></a><h1>Le site de toutes les glisses urbaines</h1>
-</div>
--- index.php
+++ index.php
@@ -1,180 +1,34 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
-	<head>
-		<title>Glisse urbaine: le site de tous ceux qui aiment le goudron: Roller, Skateboard, Trottinettes...</title>
-		<meta name="description" content="L'actualité des sports de glisse urbain: Roller, Skateboard et Trotinette. Des dossiers pour bien choisir son matériel, découvrir une nouvelle pratique, pour monter sa trottinette freestyle, ou pour voir le dernier world first en vidéo!"/>
-	<?php include("head.php"); ?>
-	<body>
-	
-	
-	<?php include("entete.php"); ?>
-	<?php include("menu.php"); ?>
-    
-	
-	<div id="conteneur">
-		<?php include("diapo2.php"); ?>
-			<div id="barmenuppal">
-				<h2 class="hidden"> Liste des dernières actualités.</h2>
-				<ul>
-					<li>
-						<div class="border">
-							<h3 class="titreSecond"><a href="24hdumans.php">Immersion dans les 24 heures du Mans Roller</a></h3>
-							<div class="content clearfix">
-								<a href="24hdumans.php" class="imagehover">
-									<img src="photos/24h2.jpg" alt="début de course 24h" class="photoarticle"/>
-									<img src="photos/fond_noir.png" class="roll" />
-								</a>
-								<p>
-									Les <strong>24 heures du Mans Roller</strong> sont maintenant, depuis 13 ans, un passage obligé pour les amateurs de Roller.
-									<br/>
-									Après une première participation il y a deux ans, nous y sommes retournés cette année avec notre bande d'amis, munis de nouvelles ambitions, je vous raconte cette belle aventure.
-								</p>
-							</div>
-						</div>
-					</li>
-					
-					
-					<li>
-						<div class="border">
-							<h3 class="titreSecond"><a href="avenirtrot.php">Quel avenir pour la trottinette freestyle?</a></h3>
-							<div class="content clearfix">
-								<a href="avenirtrot.php" class="imagehover">
-									<img src="photos/fisescooter1.jpg" alt="trottinette au FISE" class="photoarticle" title="Dossier sur l'avenir de la trottinette freestyle"/>
-									<img src="photos/fond_noir.png" class="roll" />
-								</a>
-								<p>
-									La <strong>trottinette freestyle</strong> a débarquée il y a tout juste quelques années et s'est développée à une vitesse incroyable, aujourd'hui le marché commence à saturer et la pratique se structure à peine.
-									<br/>
-									<strong>Enquête</strong> sur les perspectives de la trottinette freestyle.
-								</p>
-							</div>
-						</div>
-					</li>
-					
-					<li>
-						<div class="border">
-							<h3 class="titreSecond"><a href="powerslide.php">Powerslide: la nouvelle locomotive du marché du Roller?</a></h3>
-							<div class="content clearfix">
-								<a href="powerslide.php" class="imagehover">
-									<img src="photos/powerslide-loco2.jpg" alt="Powerslide locomotive" class="photoarticle" title="dossier Powerslide"/>
-									<img src="photos/fond_noir.png" class="roll" />
-								</a>
-								<p>
-									<strong>Powerslide</strong> est aujourd'hui la marque moteur dans un marché du Roller, qui a connu une forte baisse ces dernières années et qui aujourd'hui stagne. Dans ce contexte peu propice, Powerslide tire son épingle du jeu en innovant et en s'investissant dans tous les secteurs du roller.
-								</p>
-							</div>
-						</div>
-					</li>
-					
-					
-					<li>
-						<div class="border">
-							<h3 class="titreSecond"><a href="longboard.php">Comment choisir son longboard</a></h3>
-							<div class="content clearfix">
-								<a href="longboard.php" class="imagehover">
-									<img src="photos/longboard2.jpg" alt="longboard" class="photoarticle" title="Dossier sur comment choisir son longboard"/>
-									<img src="photos/fond_noir.png" class="roll" />
-								</a>
-								<p>
-									Le <strong>longboard</strong> se démocratise et les marques proposent maintenant des modèles adaptés à toutes les pratiques, mais difficile de trouver la board qui vous correspond. Ce guide vous aidera à vous poser les bonnes questions pour choisir la planche idéale.
-								</p>
-							</div>
-						</div>
-					</li>
-					
-					<li>
-						<div class="border">
-							<h3 class="titreSecond"><a href="montagetrot.php">Montage Trottinette</a></h3>
-							<div class="content clearfix">
-								<a href="montagetrot.php" class="imagehover">
-									<img src="photos/montageblazer2.jpg" alt="montage Trottinette" class="photoarticle" title="dossier montage trottinette"/>
-									<img src="photos/fond_noir.png" class="roll" />
-								</a>
-								<p>
-									Le <strong>guide complet</strong> pour tout savoir sur le montage de votre trottinette. Quels guidons, decks, fourches ou jeu de direction choisir?
-									<br/>
-									Un kit de compression à quoi ça sert? Comment cela se monte?
-									<br/>
-									La trottinette n'aura plus de secrets pour vous...
-								</p>
-							</div>
-						</div>
-					</li>
-					
-					<li>
-						<div class="border">
-							<h3 class="titreSecond"><a href="marques-roller.php">Le guide complet des marques de roller</a></h3>
-							<div class="content clearfix">
-								<a href="marques-roller.php" class="imagehover">
-									<img src="photos/marques-roller.jpg" alt="les marques de roller" class="photoarticle" title="Le guide des marques de roller"/>
-									<img src="photos/fond_noir.png" class="roll" />
-								</a>
-								<p>
-									Le <strong>guide complet</strong> de toutes les marques de roller: les marques historiques (Rollerblade, Fila, Roces,...), les marques qui montent (Powerslide, Seba,...) et toutes les marques de Street, Fitness, Vitesse, Roller Derby, ou Artistique!
-								</p>
-							</div>
-						</div>
-					</li>
-					<li>
-						<div class="border">
-							<h3 class="titreSecond"><a href="marques-trottinette.php">Le guide complet des marques de trottinette</a></h3>
-							<div class="content clearfix">
-								<a href="marques-trottinette.php" class="imagehover">
-									<img src="photos/marquestrot.jpg" alt="les marques de trottinette" class="photoarticle" title="Le guide des marques de trottinette"/>
-									<img src="photos/fond_noir.png" class="roll" />
-								</a>
-								<p>
-									Le <strong>guide complet</strong> de toutes les marques de trottinette: les marques historiques (Micro, Razor, JD Bug,...), les marques de freestyle australienne (Madd, Blunt, Sacrifice, ...), les nouvelles marques américaines (Lucky Scooters, Proto, Phoenix,...) ou les petites françaises (Ethic DTC, French ID, Raw,...)
-								</p>
-							</div>
-						</div>
-					</li>
-					
-					
-				</ul>
-			</div>	
-	<div id="barmenuvideo">	
-		<h3 class="titreSecond"><a href="videos.php">Vidéos +</a></h3>
-		
-		<div class="video-box">
-			<a href="videostrot.php" class="logo-video"><img src="photos/videotrottinette.jpg"  alt="logo video trottinette"/></a>
-			<iframe class="minivideo" src="https://www.youtube.com/embed/a8oiGsLog_s" frameborder="0" allowfullscreen></iframe>
-			<p>
-			<br/>
-				La dernière vidéo des petits frenchies de <a href="http://www.bluntandenvy.com/homepage/" title="vers le site"/>Blunt</a> <strong>Charles Padel</strong> et <strong>Flavio Pesenti</strong>.
-			</p>
-		</div>
-		<div id="lignemenu"></div>
-		<br/>
-		<br/>
-		<div class="video-box">
-			<a href="videosrol.php"><img src="photos/videoroller.jpg" alt="logo video roller"/></a>
-			<iframe class="minivideo" src="https://www.youtube.com/embed/HgZjYtnu4lM" frameborder="0" allowfullscreen></iframe>
-			<p>
-			<br/>	
-				<strong>Julien Cudot</strong> présente sa nouvelle marque: <a href="http://triggerskate.com/fr/" title="vers le site"/>Trigger Skates</a>.
-			</p>
-		</div>
-		<div id="lignemenu"></div>
-		<br/>
-		<br/>
-		<div class="video-box">
-			<a href="videoskate.php"><img src="photos/videoskate.jpg" alt="logo video skate"/></a>
-			<iframe class="minivideo" src="http://www.youtube.com/embed/xu6z3YbVMmk?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
-			<br/>
-			<p>
-			<br/>
-				La petite dernière de chez <a href="http://loadedboards.com/" title="vers le site"/>Loaded</a>, la <strong>Kanthaka</strong>.
-			</p>
-		</div>
-		<div id="lignemenu"></div>
-		<br/>
-		<div id="fb-video">
-			<div id="fb-root"></div><script src="http://connect.facebook.net/fr_FR/all.js#xfbml=1"></script><fb:like-box href="https://www.facebook.com/pages/Glisseurbainefr/140816476093436" width="300" show_faces="true" border_color="#ccc" stream="false" header="true"></fb:like-box>
-		</div>
-		<br/>
-	</div>
-	<?php include("pied.php"); ?>
-	</div>
-	</body>
-</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emmanuel
+ * Date: 02/01/2017
+ * Time: 16:55
+ */
+session_start();
+//ceci est le contrôleur frontal
+//il doit être short & sweet
+
+//charge nos définitions de routes
+require("app/config/routes.php");
+
+//configuration de l'appli
+require("app/config/config.php");
+
+//charge toutes nos dépendances composer
+//if (file_exists("vendor/autoload.php")){
+require("vendor/autoload.php");
+//}
+
+//auto chargement de nos classes
+spl_autoload_register(function($className){
+    $path = "app" . DIRECTORY_SEPARATOR . str_replace("\\", DIRECTORY_SEPARATOR, $className) . ".php";
+    if (file_exists($path)){
+        require($path);
+    }
+});
+
+//le dispatcher trouve la correspondance entre nos routes et l'url
+$p = (empty($_GET['p'])) ? "/" : $_GET['p'];
+$dispatcher = new Controller\Dispatcher();
+$dispatcher->dispatch($routes, $p);
